0aeae99b927a5eebd0d14edcd9f7e654c6e9d562
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-07-17 19:47:25 +0200

    weather ok(faltan css)

---
+++ src/js/weatherstation.js
@@ -0,0 +1,69 @@
+const API = 'https://api.weatherapi.com';
+const apiKey = 'd8fbab0873de40e587c151657241507';
+const currentConts = document.querySelector('.contents');
+const contForecast = document.querySelector('.contentForecast');
+//https://api.weatherapi.com/v1/current.json?key=d8fbab0873de40e587c151657241507&q=santander&aqi=no
+
+const fetchWeather = async () => {
+   
+    try {
+        const getData = await fetch(`${API}/v1/forecast.json?key=${apiKey}&q=santander&aqi=no`)
+        const data = await getData.json()
+        console.log(data);
+        
+        const dayWeather = {
+            city: data.location.name,
+            country: data.location.country,
+            weather: data.current.condition.text,
+            picture: data.current.condition.icon,
+            temperature: data.current.temp_c,
+            precipitation: data.current.precip_mm,
+            humidity: data.current.humidity,
+            wind: data.current.wind_kph,
+            forecast: data.forecast.forecastday[0].hour
+        }
+        renderWeather(dayWeather)
+        //console.log(getData);
+    } catch (error) {
+        console.log(error. message);
+    }
+} 
+
+const renderWeather = (weatherData) => {
+    //console.log(weatherData);
+    const {city, country, weather, picture, temperature, precipitation, humidity, wind, forecast} = weatherData
+    //console.log(city, country, weather, picture, temperature, precipitation, humidity, wind, forecast);
+    //console.log(forecast);
+    let elements = ''
+    const contents = `
+        <div>City:<span class="city">${city}</span></div>
+        <div>Country:<span class="country">${country}</span></div>
+        <div>Weather:<span class="weather">${weather}</span></div>
+        <div class="picture" style="width: 30px; height: 30px;">
+            <img style="width: 100%; height: 100%;" src="${picture}" alt="${city}-forecast"></div>
+            <div class="temperature">${temperature}</div>
+        </div>
+        <div>
+            <p>Precipitation:<span class="precipitation">${precipitation}</span></p>
+            <p>Humidity:<span class="humidity">${humidity}</span></p>
+            <p>Wind:<span class="wind">${wind}</span></p>
+        </div>
+        `
+        currentConts.insertAdjacentHTML('afterbegin', contents)
+
+    forecast.forEach(element => {
+        elements += `
+            <div>
+                <div class="foreHour">${element.time}</div>
+                <div style="width: 30px; height: 30px;" class="image">
+                 <img style="width: 100%; height: 100%;" src="${element.condition.icon}" alt="${city}-forecast">
+                 </div>
+                 <div class="degres">${element.temp_c}</div>
+            </div>
+            `
+    });
+
+    contForecast.innerHTML = elements
+}
+
+fetchWeather()

c9aa1f89dd77c3f73ce9af977cfb986a1c06ee8e
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-07-19 16:21:56 +0200

    scrip completado

--- src/js/securepassword.js
+++ src/js/securepassword.js
@@ -1,6 +1,6 @@
 //passwordCreate escuchador.
-const range =  document.getElementById('inputRange');
-const createPassword = document.getElementById('createPassword');
+const range =  document.querySelector('.inputRange');
+const createButton = document.getElementById('createPassword');
 const passwordCreate = document.getElementById('passwordCreate');
 
 const lowercase = 'abcdefghijklmnñopqrstuvwxyz'
@@ -8,3 +8,20 @@ const capitalLetters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
 const numbers = '0123456789'
 const symbols = '¡!#$%&/()=?¿.,+-^*ç{}[]'
 
+function createPassword(length) {
+    const allCharacters = lowercase + capitalLetters + numbers + symbols;
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allCharacters.length);
+        password += allCharacters[randomIndex];
+    }
+    return password;
+}
+
+createButton.onclick = function() {
+    const length = range.value;
+    const password = createPassword(length);
+    passwordCreate.textContent = 'Contraseña Creada: ' + password;
+    
+};

a9a2d1430aca29f5e0661125bf7e1fc4191cce62
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-07-19 12:11:08 +0200

    secure password script en progreso

---
+++ src/js/securepassword.js
@@ -0,0 +1,10 @@
+//passwordCreate escuchador.
+const range =  document.getElementById('inputRange');
+const createPassword = document.getElementById('createPassword');
+const passwordCreate = document.getElementById('passwordCreate');
+
+const lowercase = 'abcdefghijklmnñopqrstuvwxyz'
+const capitalLetters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+const numbers = '0123456789'
+const symbols = '¡!#$%&/()=?¿.,+-^*ç{}[]'
+

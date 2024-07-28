26c7b96774c6f451ca44e68a42e629e7e2979f3f
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-07-28 11:51:48 +0200

    solucion de error en background.New script con background en ruta para demas webs

---
+++ script2.js
@@ -0,0 +1,25 @@
+const imgBackground = ['../assets/img/backgroud/dogs-7275314_1280.jpg',
+'../assets/img/backgroud/boat-house-2881457_1280.jpg',
+'../assets/img/backgroud/IMG_1522.jpeg',
+'../assets/img/backgroud/geeks-2894621_1280.jpg',
+'../assets/img/backgroud/keyboard-5017973_1280.jpg',
+'../assets/img/backgroud/macbook-606763_1280.jpg',
+'../assets/img/backgroud/nature-conservation-4352793_1280.jpg',
+'../assets/img/backgroud/pc-1207886_1280.jpg',
+'../assets/img/backgroud/plans-1867745_1280.jpg',
+'../assets/img/backgroud/sea-bridge-1748162_1280.jpg',
+'../assets/img/backgroud/IMG_0163.jpeg',
+'../assets/img/backgroud/IMG_0139.jpeg',
+'../assets/img/backgroud/IMG_0080.jpeg',
+'../assets/img/backgroud/hoja-fresca-refleja-verde-vibrante-rocio-manana-generado-ia_188544-15521.jpg',
+'../assets/img/backgroud/IMG_4434.jpeg'
+]
+
+const backgroudRandom = () => {
+    const body = document.body
+    const backgroudBody = Math.floor(Math.random() * imgBackground.length)
+
+    body.style.backgroundImage = `url(${imgBackground[backgroudBody]})`
+}
+setInterval(backgroudRandom,15000)
+backgroudRandom()
